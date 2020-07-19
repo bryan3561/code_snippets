030daaf85fa10dd6dcf82255f644d4e20e64f9d2
Bryan Contreras <kennedy1991@hotmail.com>
2020-07-19 14:44:03 -0300

    add descriptions

--- python/Exceptions/exceptions.py
+++ python/Exceptions/exceptions.py
@@ -1,7 +1,15 @@
+"""
+try: The code with the exception that you want to catch
+except: This code is executed only if an exception was raised in the try block
+else: This code is executed only if no exceptions were raised in the try block
+finally: This code always executes after the other blocks
+raise("Exception"): It's used for raising your own errors
+"""
+
 try:
     f = open('curruptfile.txt')
     # if f.name == 'currupt_file.txt':
-    #     raise Exception
+    #     raise Exception("message")
 except IOError as e:
     print('First!')
 except Exception as e:
